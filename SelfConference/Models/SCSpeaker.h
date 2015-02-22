637d07b964c01a99e439ae8d2f17f7c1af10d37d
Jeff Burt <jburt1992@gmail.com>
2015-02-21 23:43:02 -0500

    Add SCSpeaker model

---
+++ SelfConference/Models/SCSpeaker.h
@@ -0,0 +1,14 @@
+//
+//  SCSpeaker.h
+//  SelfConference
+//
+//  Created by Jeff Burt on 2/21/15.
+//  Copyright (c) 2015 Self Conference. All rights reserved.
+//
+
+#import <Parse/PFObject.h>
+#import <Parse/PFSubclassing.h>
+
+@interface SCSpeaker : PFObject <PFSubclassing>
+
+@end
---
+++ SelfConference/Models/SCSpeaker.m
@@ -0,0 +1,20 @@
+//
+//  SCSpeaker.m
+//  SelfConference
+//
+//  Created by Jeff Burt on 2/21/15.
+//  Copyright (c) 2015 Self Conference. All rights reserved.
+//
+
+#import "SCSpeaker.h"
+#import <Parse/PFObject+Subclass.h>
+
+@implementation SCSpeaker
+
+#pragma mark - PFSubclassing
+
++ (NSString *)parseClassName {
+    return @"Speaker";
+}
+
+@end
