57e79950dff75d9e3a28b5ac8466cdfd0b4f60bd
Jeff Burt <jburt1992@gmail.com>
2015-02-22 00:55:24 -0500

    Create SCSessionCollectionViewCell prototype

---
+++ SelfConference/Views/SCSessionCollectionViewCell.h
@@ -0,0 +1,13 @@
+//
+//  SCSessionCollectionViewCell.h
+//  SelfConference
+//
+//  Created by Jeff Burt on 2/22/15.
+//  Copyright (c) 2015 Self Conference. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SCSessionCollectionViewCell : UICollectionViewCell
+
+@end
