f781566616060e81ed09a90301cc44b7ed4f8a98
Jeff Burt <jburt1992@gmail.com>
2015-02-21 23:23:09 -0500

    Add + connect SCSchedulePageViewController

---
+++ SelfConference/Controllers/SCSchedulePageViewController.h
@@ -0,0 +1,13 @@
+//
+//  SCSchedulePageViewController.h
+//  SelfConference
+//
+//  Created by Jeff Burt on 2/21/15.
+//  Copyright (c) 2015 Self Conference. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SCSchedulePageViewController : UIPageViewController
+
+@end
---
+++ SelfConference/Controllers/SCSchedulePageViewController.m
@@ -0,0 +1,13 @@
+//
+//  SCSchedulePageViewController.m
+//  SelfConference
+//
+//  Created by Jeff Burt on 2/21/15.
+//  Copyright (c) 2015 Self Conference. All rights reserved.
+//
+
+#import "SCSchedulePageViewController.h"
+
+@implementation SCSchedulePageViewController
+
+@end
