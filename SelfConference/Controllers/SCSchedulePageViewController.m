506658d33414d3107a136613bf0ca9195d5b5703
Jeff Burt <jburt1992@gmail.com>
2015-02-21 23:29:31 -0500

    Add two days to the schedule

--- SelfConference/Controllers/SCSchedulePageViewController.m
+++ SelfConference/Controllers/SCSchedulePageViewController.m
@@ -7,7 +7,72 @@
 //
 
 #import "SCSchedulePageViewController.h"
+#import "SCDayScheduleTableViewController.h"
+
+@interface SCSchedulePageViewController () <UIPageViewControllerDataSource, UIPageViewControllerDelegate>
+
+/** A collection of the view controllers that will exist in the specific order */
+@property (nonatomic, strong) NSMutableArray *viewControllersToUse;
+
+@end
 
 @implementation SCSchedulePageViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.delegate = self;
+    self.dataSource = self;
+    
+    [self setViewControllers:@[self.viewControllersToUse.firstObject]
+                   direction:UIPageViewControllerNavigationDirectionForward
+                    animated:NO
+                  completion:NULL];
+}
+
+- (NSMutableArray *)viewControllersToUse {
+    if (!_viewControllersToUse) {
+        NSMutableArray *viewControllersToUse = [NSMutableArray array];
+        
+        // Day one
+        [viewControllersToUse addObject:[SCDayScheduleTableViewController new]];
+
+        // Day two
+        [viewControllersToUse addObject:[SCDayScheduleTableViewController new]];
+        
+        _viewControllersToUse = viewControllersToUse;
+    }
+    
+    return _viewControllersToUse;
+}
+
+#pragma mark - UIPageViewControllerDataSource
+
+- (UIViewController *)pageViewController:(UIPageViewController *)pageViewController
+      viewControllerBeforeViewController:(UIViewController *)viewController {
+    UIViewController *previousViewController;
+    
+    NSInteger previousIndex =
+    [self.viewControllersToUse indexOfObject:viewController] - 1;
+    
+    if (self.viewControllersToUse.count > previousIndex && previousIndex >= 0) {
+        previousViewController = self.viewControllersToUse[previousIndex];
+    }
+    
+    return previousViewController;
+}
+
+- (UIViewController *)pageViewController:(UIPageViewController *)pageViewController
+       viewControllerAfterViewController:(UIViewController *)viewController {
+    UIViewController *nextViewController;
+    
+    NSInteger nextIndex = [self.viewControllersToUse indexOfObject:viewController] + 1;
+    
+    if (self.viewControllersToUse.count > nextIndex) {
+        nextViewController = self.viewControllersToUse[nextIndex];
+    }
+    
+    return nextViewController;
+}
 
 @end
