3d18c0b3d4f75d9b4756f4e6e9ef2e4309607b6a
Jeff Burt <jburt1992@gmail.com>
2015-02-21 23:41:10 -0500

    Add SCSession model

---
+++ SelfConference/Models/SCSession.h
@@ -0,0 +1,14 @@
+//
+//  SCSession.h
+//  SelfConference
+//
+//  Created by Jeff Burt on 2/21/15.
+//  Copyright (c) 2015 Self Conference. All rights reserved.
+//
+
+#import <Parse/PFObject.h>
+#import <Parse/PFSubclassing.h>
+
+@interface SCSession : PFObject <PFSubclassing>
+
+@end
---
+++ SelfConference/Models/SCSession.m
@@ -0,0 +1,20 @@
+//
+//  SCSession.m
+//  SelfConference
+//
+//  Created by Jeff Burt on 2/21/15.
+//  Copyright (c) 2015 Self Conference. All rights reserved.
+//
+
+#import "SCSession.h"
+#import <Parse/PFObject+Subclass.h>
+
+@implementation SCSession
+
+#pragma mark - PFSubclassing
+
++ (NSString *)parseClassName {
+    return @"Session";
+}
+
+@end
