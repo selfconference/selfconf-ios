c0334d7fa9f6134206090b415787bb65fc11953b
Jeff Burt <jburt1992@gmail.com>
2015-02-21 23:23:29 -0500

    Created SCDayScheduleTableViewController

---
+++ SelfConference/Views/SCDayScheduleTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  SCDayScheduleTableViewController.h
+//  SelfConference
+//
+//  Created by Jeff Burt on 2/21/15.
+//  Copyright (c) 2015 Self Conference. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SCDayScheduleTableViewController : UITableViewController
+
+@end
---
+++ SelfConference/Views/SCDayScheduleTableViewController.m
@@ -0,0 +1,13 @@
+//
+//  SCDayScheduleTableViewController.m
+//  SelfConference
+//
+//  Created by Jeff Burt on 2/21/15.
+//  Copyright (c) 2015 Self Conference. All rights reserved.
+//
+
+#import "SCDayScheduleTableViewController.h"
+
+@implementation SCDayScheduleTableViewController
+
+@end
