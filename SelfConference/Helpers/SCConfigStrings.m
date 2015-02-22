409013c518ac34123b359898ff8ba241717d1cce
Jeff Burt <jburt1992@gmail.com>
2015-02-22 01:46:42 -0500

    Created SCConfigStrings

---
+++ SelfConference/Helpers/SCConfigStrings.m
@@ -0,0 +1,13 @@
+//
+//  SCConfigStrings.m
+//  SelfConference
+//
+//  Created by Jeff Burt on 2/22/15.
+//  Copyright (c) 2015 Self Conference. All rights reserved.
+//
+
+#import "SCConfigStrings.h"
+
+const struct SCConfigStrings SCConfigStrings = {
+    .conferenceStartTime = @"conferenceStartTime"
+};
