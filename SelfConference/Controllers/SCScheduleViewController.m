be9a46abfd923af38695c0b9f10e5ca150c52e5b
Jeff Burt <jburt1992@gmail.com>
2015-03-21 17:35:14 -0400

    Add day buttons to the Schedule view

--- SelfConference/Controllers/SCScheduleViewController.m
+++ SelfConference/Controllers/SCScheduleViewController.m
@@ -14,6 +14,8 @@
 #import <MTDates/NSDate+MTDates.h>
 #import "UIViewController+SCChildViewController.h"
 #import "UIColor+SCColor.h"
+#import "SCDaySelectionButton.h"
+#import "NSDate+SCDate.h"
 
 @interface SCScheduleViewController () <UIPageViewControllerDataSource, UIPageViewControllerDelegate>
 
@@ -27,6 +29,10 @@
 
 /** A collection of the view controllers that will exist in the specific order */
 @property (nonatomic, strong) NSMutableArray *viewControllersToUse;
+
+@property (weak, nonatomic) IBOutlet SCDaySelectionButton *firstDayButton;
+
+@property (weak, nonatomic) IBOutlet SCDaySelectionButton *secondDayButton;
 
 @end
 
@@ -42,8 +48,16 @@
     navigationBar.titleTextAttributes =
     @{NSForegroundColorAttributeName: [UIColor whiteColor]};
     
+    [self setDate:[NSDate SC_firstDayOfConference]
+     forDayButton:self.firstDayButton];
+    
+    [self setDate:[NSDate SC_secondDayOfConference]
+     forDayButton:self.secondDayButton];
+    
     [self SC_addChildViewController:self.pageViewController
                         onTopOfView:self.pageViewControllerContainerView];
+    
+    [self scrollToViewControllerForSelectedDayAnimated:NO];
 }
 
 #pragma mark - Lazy loading
@@ -55,12 +69,6 @@
          initWithTransitionStyle:UIPageViewControllerTransitionStyleScroll
          navigationOrientation:UIPageViewControllerNavigationOrientationHorizontal
          options:kNilOptions];
-        
-        [_pageViewController
-         setViewControllers:@[self.viewControllersToUse.firstObject]
-         direction:UIPageViewControllerNavigationDirectionForward
-         animated:NO
-         completion:NULL];
         
         _pageViewController.delegate = self;
         _pageViewController.dataSource = self;
@@ -115,7 +123,30 @@
     
     return nextViewController;
 }
-         
+
+- (void)pageViewController:(UIPageViewController *)pageViewController
+        didFinishAnimating:(BOOL)finished
+   previousViewControllers:(NSArray *)previousViewControllers
+       transitionCompleted:(BOOL)completed {
+    if (completed) {
+        [self toggleSelectedDayButtonsAndScrollToRelatedViewController:NO];
+    }
+}
+
+#pragma mark - UIButton actions
+
+- (IBAction)didTapFirstDayButton:(SCDaySelectionButton *)button {
+    if ([self shouldToggleSelectedDayButtonsWithDayButtonTap:button]) {
+        [self toggleSelectedDayButtonsAndScrollToRelatedViewController:YES];
+    }
+}
+
+- (IBAction)didTapSecondDayButton:(SCDaySelectionButton *)button {
+    if ([self shouldToggleSelectedDayButtonsWithDayButtonTap:button]) {
+        [self toggleSelectedDayButtonsAndScrollToRelatedViewController:YES];
+    }
+}
+
 #pragma mark - Other
 
 /** 
@@ -133,6 +164,58 @@
     dayScheduleCollectionViewController.startOfDay = startOfDay;
     
     return dayScheduleCollectionViewController;
+}
+
+/** Sets the title on 'dayButton' to the name of the day. */
+- (void)setDate:(NSDate *)date forDayButton:(SCDaySelectionButton *)dayButton {
+    [dayButton
+     setTitle:date.mt_stringFromDateWithFullWeekdayTitle.uppercaseString
+     forState:UIControlStateNormal];
+}
+
+/** 
+ Returns YES if the 'dayButton' that trigger all of the day buttons to be 
+ toggled.
+ */
+- (BOOL)shouldToggleSelectedDayButtonsWithDayButtonTap:(SCDaySelectionButton *)dayButton {
+    return !dayButton.selected;
+}
+
+/** 
+ Switches the selected state for '_firstDayButton' and '_secondDayButton' and
+ scrolls to the related view controller if desired.
+ */
+- (void)toggleSelectedDayButtonsAndScrollToRelatedViewController:(BOOL)shouldScrollToRelatedViewController {
+    self.firstDayButton.selected = !self.firstDayButton.selected;
+    self.secondDayButton.selected = !self.secondDayButton.selected;
+    
+    if (shouldScrollToRelatedViewController) {
+        [self scrollToViewControllerForSelectedDayAnimated:YES];
+    }
+}
+
+/**
+ Scrolls to the page for the given selected day. To be called when the user taps 
+ on one of the dayButton's.
+ */
+- (void)scrollToViewControllerForSelectedDayAnimated:(BOOL)shouldAnimate {
+    UIViewController *pageForSelectedDayButton;
+    UIPageViewControllerNavigationDirection scrollDirection;
+    
+    if (self.firstDayButton.isSelected) {
+        pageForSelectedDayButton = self.viewControllersToUse.firstObject;
+        scrollDirection = UIPageViewControllerNavigationDirectionReverse;
+    }
+    else {
+        pageForSelectedDayButton = self.viewControllersToUse.lastObject;
+        scrollDirection = UIPageViewControllerNavigationDirectionForward;
+    }
+    
+    [self.pageViewController
+     setViewControllers:@[pageForSelectedDayButton]
+     direction:scrollDirection
+     animated:shouldAnimate
+     completion:NULL];
 }
 
 @end
