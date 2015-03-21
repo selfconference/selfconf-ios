f932267e3bef3bfc460db716187506e47abcd59c
Jeff Burt <jburt1992@gmail.com>
2015-03-21 16:08:11 -0400

    Created UIColor+SCColor with custom colors

---
+++ SelfConference/Categories/UIColor+SCColor.h
@@ -0,0 +1,19 @@
+//
+//  UIColor+SCColor.h
+//  SelfConference
+//
+//  Created by Jeff Burt on 3/21/15.
+//  Copyright (c) 2015 Self Conference. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (SCColor)
+
++ (instancetype)SC_teal;
++ (instancetype)SC_orange;
++ (instancetype)SC_purple;
++ (instancetype)SC_yellow;
++ (instancetype)SC_red;
+
+@end
---
+++ SelfConference/Categories/UIColor+SCColor.m
@@ -0,0 +1,50 @@
+//
+//  UIColor+SCColor.m
+//  SelfConference
+//
+//  Created by Jeff Burt on 3/21/15.
+//  Copyright (c) 2015 Self Conference. All rights reserved.
+//
+
+#import "UIColor+SCColor.h"
+
+@implementation UIColor (SCColor)
+
++ (instancetype)SC_teal {
+    return
+    [self SC_solidColorWithNumeratorsForRed:51 green:145 blue:148];
+}
+
++ (instancetype)SC_orange {
+    return
+    [self SC_solidColorWithNumeratorsForRed:251 green:107 blue:64];
+}
+
++ (instancetype)SC_purple {
+    return
+    [self SC_solidColorWithNumeratorsForRed:167 green:2 blue:103];
+}
+
++ (instancetype)SC_yellow {
+    return
+    [self SC_solidColorWithNumeratorsForRed:246 green:216 blue:107];
+}
+
++ (instancetype)SC_red {
+    return
+    [self SC_solidColorWithNumeratorsForRed:241 green:13 blue:73];
+}
+
+#pragma mark - Internal
+
+/** Returns an opaque color with the given RGB numerators. */
++ (instancetype)SC_solidColorWithNumeratorsForRed:(NSInteger)red
+                                            green:(NSInteger)green
+                                             blue:(NSInteger)blue {
+    return [self colorWithRed:red / 255.0f
+                        green:green / 255.0f
+                         blue:blue / 255.0f
+                        alpha:1.0f];
+}
+
+@end
