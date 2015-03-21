9b480f7982a75da591623a80ff018afe0c712b77
Jeff Burt <jburt1992@gmail.com>
2015-03-21 17:33:45 -0400

    Add SCFadeWhenHighlightedButton
    A button that fades when highlighted (i.e. when tapped). Works if the
    button is created in code or if it was created in Interface Builder.

---
+++ SelfConference/Views/SCFadeWhenHighlightedButton.h
@@ -0,0 +1,17 @@
+//
+//  SCFadeWhenHighlightedButton.h
+//  SelfConference
+//
+//  Created by Jeff Burt on 3/21/15.
+//  Copyright (c) 2015 Self Conference. All rights reserved.
+//
+
+@import UIKit;
+
+/** 
+ A button that fades when highlighted (i.e. when tapped). Works if the
+ button is created in code or if it was created in Interface Builder.
+ */
+@interface SCFadeWhenHighlightedButton : UIButton
+
+@end
