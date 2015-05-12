fe010e49cf3cf3ab77e655211385ccd024d4d4c0
Jeff Burt <jburt1992@gmail.com>
2015-05-12 00:27:03 -0400

    Set the custom date format on all date fields
    That way dates actually get imported

--- SelfConference/Models/SCManagedObject/SCManagedObject.m
+++ SelfConference/Models/SCManagedObject/SCManagedObject.m
@@ -7,8 +7,20 @@
 //
 
 #import "SCManagedObject.h"
+#import "NSAttributeDescription+SCAttributeDescription.h"
 
 @implementation SCManagedObject
+
+- (instancetype)initWithEntity:(NSEntityDescription *)entity
+insertIntoManagedObjectContext:(NSManagedObjectContext *)context {
+    self = [super initWithEntity:entity insertIntoManagedObjectContext:context];
+    
+    if (self) {
+        [self setDateFormatForAllDateAttributes];
+    }
+    
+    return self;
+}
 
 #pragma mark - Overridden
 
@@ -23,6 +35,26 @@
                                                 withString:@""
                                                    options:0
                                                      range:NSMakeRange(0, classPrefix.length)];
+}
+
+#pragma mark - Internal
+
+/**
+ Loops through each NSDateAttributeType attribute and applies the date format
+ that is used by the API.
+ */
+- (void)setDateFormatForAllDateAttributes {
+    for (id property in self.entity.properties) {
+        if ([property isKindOfClass:[NSAttributeDescription class]]) {
+            NSAttributeDescription *attributeDescription = property;
+            
+            if (attributeDescription.attributeType == NSDateAttributeType) {
+                // Example date string from the API: @"2015-03-02T14:50:47.944Z"
+                [attributeDescription
+                 SC_setDateFormat:@"yyyy-MM-dd'T'HH:mm:ss.SSSz"];
+            }
+        }
+    }
 }
 
 @end
