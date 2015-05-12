cadca87ec2edf48bcc62202798c4b697556abe09
Jeff Burt <jburt1992@gmail.com>
2015-05-11 23:38:00 -0400

    Add internal GET request helpers

--- SelfConference/API/SCAPIService.m
+++ SelfConference/API/SCAPIService.m
@@ -7,6 +7,11 @@
 //
 
 #import "SCAPIService.h"
+#import "SCHTTPSessionManager.h"
+#import "SCEvent.h"
+
+typedef void (^SCAPIServiceTaskWithResponseObjectBlock)(NSURLSessionDataTask *task, id responseObject);
+typedef void (^SCAPIServiceTaskWithErrorBlock)(NSURLSessionDataTask *task, NSError *error);
 
 @implementation SCAPIService
 
@@ -18,6 +23,42 @@
     });
     
     return sharedInstance;
+}
+
+#pragma mark - Internal
+
+/**
+ Calls a SCAPIServiceResponseObjectWithErrorBlock if it exists with the given
+ parameters.
+ */
+- (void)callSCAPIServiceResponseObjectWithErrorBlock:(SCAPIServiceResponseObjectWithErrorBlock)block
+                                      responseObject:(id)responseObject
+                                               error:(NSError *)error {
+    if (block) {
+        block(responseObject, error);
+    }
+    else {
+        NSLog(@"SCAPIServiceResponseObjectWithErrorBlock is nil");
+    }
+}
+
+/** Custom HTTP get method that returns a BTAPIResponse in the success block */
+- (NSURLSessionDataTask *)GET:(NSString *)URLString
+                   parameters:(id)parameters
+              completionBlock:(SCAPIServiceResponseObjectWithErrorBlock)completionBlock {
+    return [[SCHTTPSessionManager sharedInstance]
+            GET:URLString
+            parameters:parameters
+            success:^(NSURLSessionDataTask *task, id responseObject) {
+                [self callSCAPIServiceResponseObjectWithErrorBlock:completionBlock
+                                                    responseObject:responseObject
+                                                             error:nil];
+            }
+            failure:^(NSURLSessionDataTask *task, NSError *error) {
+                [self callSCAPIServiceResponseObjectWithErrorBlock:completionBlock
+                                                    responseObject:nil
+                                                             error:error];
+            }];
 }
 
 @end
