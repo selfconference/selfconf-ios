bcbe5fed6771fe892afd17205a6a4c6c45db5961
Jeff Burt <jburt1992@gmail.com>
2015-05-11 22:57:07 -0400

    Added SCAPIRelativeUrlStrings struct

---
+++ SelfConference/API/SCAPIStrings.h
@@ -0,0 +1,20 @@
+//
+//  SCAPIStrings.h
+//  SelfConference
+//
+//  Created by Jeff Burt on 5/11/15.
+//  Copyright (c) 2015 Self Conference. All rights reserved.
+//
+
+@import Foundation;
+
+extern const struct SCAPIRelativeUrlStrings {
+    __unsafe_unretained NSString *events;
+    __unsafe_unretained NSString *venues;
+    __unsafe_unretained NSString *sessions;
+    __unsafe_unretained NSString *rooms;
+    __unsafe_unretained NSString *speakers;
+    __unsafe_unretained NSString *sponsors;
+    __unsafe_unretained NSString *sponsorLevels;
+    __unsafe_unretained NSString *organizers;
+} SCAPIRelativeUrlStrings;
---
+++ SelfConference/API/SCAPIStrings.m
@@ -0,0 +1,20 @@
+//
+//  SCAPIStrings.m
+//  SelfConference
+//
+//  Created by Jeff Burt on 5/11/15.
+//  Copyright (c) 2015 Self Conference. All rights reserved.
+//
+
+#import "SCAPIStrings.h"
+
+const struct SCAPIRelativeUrlStrings SCAPIRelativeUrlStrings = {
+    .events = @"events",
+    .venues = @"venues",
+    .sessions = @"sessions",
+    .rooms = @"rooms",
+    .speakers = @"speakers",
+    .sponsors = @"sponsors",
+    .sponsorLevels = @"sponsor_levels",
+    .organizers = @"organizers"
+};
