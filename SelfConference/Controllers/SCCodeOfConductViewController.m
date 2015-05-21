8910ab35e7e3be3029828d89d05a2a0e9c2d287d
Jeff Burt <jburt1992@gmail.com>
2015-05-20 23:20:10 -0400

    Make sure code of conduct is scrolled to the top on load

--- SelfConference/Controllers/SCCodeOfConductViewController.m
+++ SelfConference/Controllers/SCCodeOfConductViewController.m
@@ -9,12 +9,25 @@
 #import "SCCodeOfConductViewController.h"
 #import "UIColor+SCColor.h"
 
+@interface SCCodeOfConductViewController ()
+
+@property (weak, nonatomic) IBOutlet UITextView *textView;
+
+@end
+
 @implementation SCCodeOfConductViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     
     self.view.backgroundColor = [UIColor SC_offWhite];
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    
+    // Sometimes the text view isn't scrolled to the very top right away
+    [self.textView setContentOffset:CGPointZero animated:YES];
 }
 
 - (IBAction)didTapCloseBarButtonItem:(UIBarButtonItem *)sender {
