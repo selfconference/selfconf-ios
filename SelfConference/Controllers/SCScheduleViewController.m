6e9fa059da28b4fb0a99ff5908de3ed068f1dfb4
Jeff Burt <jburt1992@gmail.com>
2015-05-20 23:08:12 -0400

    Remove debug code that snuck in

--- SelfConference/Controllers/SCScheduleViewController.m
+++ SelfConference/Controllers/SCScheduleViewController.m
@@ -91,9 +91,7 @@
     
     // Disable scrolling by default, until it becomes exposed
     cell.tableView.scrollEnabled = NO;
-    
-    NSLog(@"Section %d", indexPath.section);
-    
+        
     cell.session = self.sessions[indexPath.row];
     
     return cell;
