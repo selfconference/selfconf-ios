87e15cd887a71797b3942c1f82ba14cbb14d9605
FlaviusBortas <flaviusbortas@gmail.com>
2019-05-26 14:51:50 -0400

    Converting UIColors to Swift

---
+++ SelfConference/Categories/UIColor+SCColor.swift
@@ -0,0 +1,35 @@
+//
+//  UIColor+SCColor.swift
+//  SelfConference
+//
+//  Created by Flavius on 5/26/19.
+//  Copyright © 2019 Self Conference. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    
+    @objc(SC_teal)
+    static let scTeal  = UIColor(named: "SC_teal")!
+    
+    @objc(SC_darkTeal)
+    static let scDarkTeal  = UIColor(named: "SC_darkTeal")!
+    
+    @objc(SC_offWhite)
+    static let scOffWhite = UIColor(named: "SC_offWhite")!
+    
+    @objc(SC_orange)
+    static let scOrange = UIColor(named: "SC_orange")!
+    
+    @objc(SC_purple)
+    static let scPurple = UIColor(named: "SC_purple")!
+    
+    @objc(SC_red)
+    static let scRed = UIColor(named: "SC_red")!
+    
+    @objc(SC_yellow)
+    static let scYellow = UIColor(named: "SC_yellow")!
+    
+}
