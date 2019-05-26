e5c307164f456d07d59126e2175c3ec7c27b4027
FlaviusBortas <flaviusbortas@gmail.com>
2019-05-26 16:03:10 -0400

    Adding constants object
    All constants will be grouped into this object for ease of access.

---
+++ SelfConference/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  SelfConference
+//
+//  Created by Flavius on 5/26/19.
+//  Copyright © 2019 Self Conference. All rights reserved.
+//
+
+import Foundation
+
+// Obj-C does not interop with enums or structs, this class is used as a constants storage. Once the project is fully converted to swift, this object can be converted to an enum. At that point the init method and @objc annotations can also be removed.
+
+class Constants: NSObject {
+    // Preventing this class from being initalized
+    private override init() {}
+    
+}
