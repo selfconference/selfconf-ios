8a144ffbd5ba8fc6686dfbabf31a1e7bf81b63fc
FlaviusBortas <flaviusbortas@gmail.com>
2019-05-26 16:07:15 -0400

    Declaring Constants as final
    The Constants object should never be subclassed, marking it as final to prevent subclassing and to impove optimization.

--- SelfConference/Constants.swift
+++ SelfConference/Constants.swift
@@ -10,7 +10,7 @@ import Foundation
 
 // Obj-C does not interop with enums or structs, this class is used as a constants storage. Once the project is fully converted to swift, this object can be converted to an enum. At that point the init method and @objc annotations can also be removed.
 
-class Constants: NSObject {
+final class Constants: NSObject {
     // Preventing this class from being initalized
     private override init() {}
     
