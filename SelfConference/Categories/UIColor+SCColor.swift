c4e800400f47de46f88845b6394e266dd6d65c43
FlaviusBortas <flaviusbortas@gmail.com>
2019-05-29 11:38:08 -0400

    Added default colors via nil coalescing
    Nil Coalescing was done here to provide a default color in case of any crashes happen

--- SelfConference/Categories/UIColor+SCColor.swift
+++ SelfConference/Categories/UIColor+SCColor.swift
@@ -12,24 +12,24 @@ import UIKit
 extension UIColor {
     
     @objc(SC_teal)
-    static let scTeal  = UIColor(named: "SC_teal")!
+    static let scTeal  = UIColor(named: "SC_teal") ?? UIColor(red: 51, green: 145, blue: 148, alpha: 1)
     
     @objc(SC_darkTeal)
-    static let scDarkTeal  = UIColor(named: "SC_darkTeal")!
+    static let scDarkTeal  = UIColor(named: "SC_darkTeal") ?? UIColor(red: 45, green: 130, blue: 133, alpha: 1)
     
     @objc(SC_offWhite)
-    static let scOffWhite = UIColor(named: "SC_offWhite")!
+    static let scOffWhite = UIColor(named: "SC_offWhite") ?? UIColor(red: 245, green: 244, blue: 244, alpha: 1)
     
     @objc(SC_orange)
-    static let scOrange = UIColor(named: "SC_orange")!
+    static let scOrange = UIColor(named: "SC_orange") ?? UIColor(red: 251, green: 107, blue: 64, alpha: 1)
     
     @objc(SC_purple)
-    static let scPurple = UIColor(named: "SC_purple")!
+    static let scPurple = UIColor(named: "SC_purple") ?? UIColor(red: 167, green: 2, blue: 103, alpha: 1)
     
     @objc(SC_red)
-    static let scRed = UIColor(named: "SC_red")!
+    static let scRed = UIColor(named: "SC_red") ?? UIColor(red: 241, green: 13, blue: 73, alpha: 1)
     
     @objc(SC_yellow)
-    static let scYellow = UIColor(named: "SC_yellow")!
+    static let scYellow = UIColor(named: "SC_yellow") ?? UIColor(red: 246, green: 216, blue: 107, alpha: 1)
     
 }
